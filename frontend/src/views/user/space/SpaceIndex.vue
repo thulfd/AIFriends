70c9a17f5671bedd5279b3f33f58e7d3598415bc
l fd <840377552@qq.com>
2026-04-01 21:57:14 +0800

    实现路由

---
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {useRoute, useRouter} from "vue-router";
+
+const route = useRoute()
+const router = useRouter()
+</script>
+
+<template>
+个人空间:{{route.params.user_id}}
+
+
+</template>
+
+<style scoped>
+
+</style>
